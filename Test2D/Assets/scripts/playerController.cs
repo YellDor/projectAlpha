c561f5381dbfd4048175fb172fa542791c3be8e7
YellDor <officialyelldor@gmail.com>
2017-05-19 22:29:12 +0100

    game testing added

---
+++ Test2D/Assets/scripts/playerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// movement variables
+	public float maxSpeed;
+
+	Rigidbody2D myRB;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+
+		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
+	}
+}
